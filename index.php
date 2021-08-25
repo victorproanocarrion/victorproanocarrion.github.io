8bfbb08dfb8967eba6f684e9834eedf5cf51baa5
victorproanocarrion <89489426+victorproanocarrion@users.noreply.github.com>
2021-08-24 20:04:59 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>INGRESO DE ASISTENCIA</title>
+
+<meta charset="utf-8">
+
+</head>
+
+<body>
+
+<form name="formulario" method="post" action="asistencia.php">
+
+Nombre: <input type="text" name="nombre" value="">
+
+<input type="submit" />
+
+</form>
+
+</body>
+
+</html>
