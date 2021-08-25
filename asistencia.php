35bf1028ded3cb92b43d3382b689bfafcdc5e357
victorproanocarrion <89489426+victorproanocarrion@users.noreply.github.com>
2021-08-24 20:02:08 -0500

    Update asistencia.php

--- asistencia.php
+++ asistencia.php
@@ -1,27 +1,9 @@
-<html>
-<head>
 <?php
-$nombre=$_POST['select'];
-$personal=$_POST['pe'];
-$es=$_POST['in'];
-$fecha=strftime("%D");
-$hora= strftime("%T");
-$registro = "
-Nombre:$nombre
-Personal:$personal
-ES:$es
-Hora:$hora";
 
-$archivo = fopen("$fecha.txt","a");
-fwrite($archivo;$registro); 
+$nombre = $_POST['nombre'];
 
+echo $nombre;
 
 ?>
-</head>
-<body>
-<div align="center">
-<h2> Datos ingresados correctamente </h2>
-</div>
-</body>
-</html>
+
 
